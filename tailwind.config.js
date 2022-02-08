93bfe466a73b32912b6d52e78d106790cb8656ca
bangkitwahyus <bangkitwahyusaputra@gmail.com>
2022-02-08 16:49:39 +0700

    belajar tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      spacing: {
+        13: "3.25rem",
+      },
+      fontFamily: {
+        inter: ["Inter"],
+      },
+    },
+  },
+  plugins: [],
+};
