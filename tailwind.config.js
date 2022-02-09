f01a43e96dba527d5e06c81f092012fbb9d1ae09
bangkitwahyus <bangkitwahyusaputra@gmail.com>
2022-02-09 13:26:19 +0700

    Transform Transition dan Animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,20 @@ module.exports = {
       fontFamily: {
         inter: ["Inter"],
       },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        shake: "shake 1s ease-in-out infinite",
+      },
+      keyframes: {
+        shake: {
+          "0%, 100%": {
+            transform: "rotate(-3deg)",
+          },
+          "50%": {
+            transform: "rotate(3deg)",
+          },
+        },
+      },
     },
   },
   plugins: [],
